37ee9c0d563fdb5135c8091b6e366b2ddcd585f4
Daniel <goldstei1@grinnell.edu>
2013-09-04 14:56:21 -0500

    Changed output

--- src/edu/grinnell/csc207/goldstei1/hello/HelloGit.java
+++ src/edu/grinnell/csc207/goldstei1/hello/HelloGit.java
@@ -2,6 +2,6 @@ package edu.grinnell.csc207.goldstei1.hello;
 
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello, EGit!");
+        System.out.println("Hello, EGit again!");
     } // main(String[])
 }
