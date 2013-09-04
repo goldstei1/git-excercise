516e6d2212f59579a3bad9c235d88e0b5c07641b
Daniel Goldstein <dpgold94@gmail.com>
2013-09-04 10:48:28 -0500

    Added HelloWorld.java

---
+++ src/edu/grinnell/csc207/goldstei1/hello/HelloWorld.java
@@ -0,0 +1,7 @@
+package edu.grinnell.csc207.goldstei1.hello;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+        System.out.println("Hello, GitHub!");
+    } // main(String[])
+}
