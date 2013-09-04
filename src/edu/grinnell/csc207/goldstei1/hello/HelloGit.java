df525851bd74496f889d049613f6c421fe440d78
Daniel <goldstei1@grinnell.edu>
2013-09-04 14:54:05 -0500

    Added HelloGit.java

---
+++ src/edu/grinnell/csc207/goldstei1/hello/HelloGit.java
@@ -0,0 +1,7 @@
+package edu.grinnell.csc207.goldstei1.hello;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello, EGit!");
+    } // main(String[])
+}
