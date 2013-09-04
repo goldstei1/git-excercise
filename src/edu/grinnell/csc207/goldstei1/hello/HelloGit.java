b049c8ab557af7c593c4c8fbe84e688b3048404b
Daniel <goldstei1@grinnell.edu>
2013-09-04 15:01:25 -0500

    Test to see if still working

--- src/edu/grinnell/csc207/goldstei1/hello/HelloGit.java
+++ src/edu/grinnell/csc207/goldstei1/hello/HelloGit.java
@@ -3,6 +3,6 @@ package edu.grinnell.csc207.goldstei1.hello;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello, EGit again!");
-        System.out.println("This will break");
+        System.out.println("This will not break");
     } // main(String[])
 }
